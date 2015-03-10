d73c67f3b454bc8630537eef1463abc67c0e059f
Matt Croydon <mattcroydon8@gmail.com>
2015-03-10 19:03:02 +0000

    Socket.io Improvements

--- app.js
+++ app.js
@@ -6,73 +6,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var io = require('./ioapp');
 
-//var util = require('util');
 
 var index = require('./routes/index');
 var league = require('./routes/league');
 var team = require('./routes/team');
 var leagueTeams = require('./routes/leagueTeams');
 var graphs = require('./routes/graphs');
-//var playerGraph = require('./routes/playerGraph');
 
 var app = express();
-
-/*
-//ioServer
-var ioapp = express().createServer();
-var io = require('socket.io')(app);
-
-ioapp.listen(80);
-
-ioapp.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-*/
-
-var io = require('socket.io').listen(3001);
-/*
-io.sockets.on('connection', function (socket) {
-  socket.on('message', function (message) {
-    console.log("Got message: " + message);
-    io.sockets.emit('pageview', { 'url': message });
-  });
-});*/
-/*
-io.configure(function () {
-  io.set('authorization', function (handshakeData, callback) {
-    if (handshakeData.xdomain) {
-      callback('Cross-domain connections are not allowed');
-    } else {
-      callback(null, true);
-    }
-  });
-});
-*/
-io.sockets.on('connection', function (socket) {
-
-  socket.on('message', function (message) {
-
-    ip = socket.request.connection.remoteAddress;
-    url = message;
-    console.log("Client:" + ip);
-    io.sockets.emit('pageview', {'connections': io.engine.clientsCount, 'ip': ip, 'url': url, 'xdomain': socket.handshake.xdomain, 'timestamp': new Date()});
-  });
-
-  socket.on('disconnect', function () {
-    console.log("Socket disconnected");
-    io.sockets.emit('disconnect', { 'connections': io.engine.clientsCount});
-  });
-
-});
-
 
 app.use(favicon(path.join(__dirname,'public','images','raspberrypi.ico')));
 
